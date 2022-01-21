701a0d2d294d3f2a24dc7f2a1ce8f07193c9d8ff
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-20 23:58:21 -0600

    minimal changes with react-router

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 function HomePage() {
     return (
         <div className="App">
             <section className="App-header">
-                {/* <ListTasks/> */}
+                <Outlet/>
             </section>
         </div >
     );
