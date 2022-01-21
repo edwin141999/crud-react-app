b34b5414e7777c94b26a162cf9f9a31d72cfd2a6
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-20 23:57:54 -0600

    connect with db

--- src/pages/CreateTask.js
+++ src/pages/CreateTask.js
@@ -1,11 +1,29 @@
+import { useForm } from "react-hook-form";
+import { NavLink, useNavigate } from "react-router-dom";
+import { addTask } from "../services/db";
+
 function CreateTask() {
+    const { register, formState: { errors }, handleSubmit } = useForm()
+    const navigate = useNavigate()
+    const onSubmit = (data) => {
+        console.log(data);
+        addTask(data.title,data.description)
+        navigate('/listTasks')
+    }
+
     return (
         <div>
-            <h4>Titulo</h4>
-            <input type={"text"}></input>
-            <h3>Descripcion</h3>
-            <input type={"text"}></input>
-            <button>Crear</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h4>Titulo</h4>
+                <input type={"text"} {...register("title", { required: true })} />
+                <p>{errors.title?.type === 'required' && 'Se requiere un titulo'}</p>
+                <h4>Descripcion</h4>
+                <input type={"text"} {...register("description", { required: true })} />
+                <p>{errors.description?.type === 'required' && 'Se requiere una descripcion'}</p>
+                <NavLink to='/listTasks' >
+                </NavLink>
+                <button type="submit">Crear</button>
+            </form>
         </div>);
 }
 
