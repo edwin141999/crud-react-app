cdbe33aa21bcc8ba8c09351681f427091e74ad87
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-20 23:56:00 -0600

    create routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import CreateTask from "./pages/CreateTask";
 import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from './components/Navbar';
@@ -13,12 +13,13 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/createTask' element={<CreateTask />} />
-        {/* <Route path='/updateTask' element={<UpdateTask />} /> */}
-        <Route path='/listTasks' element={<ListTasks />} />
-        <Route path='*' element={<NotFoundPage />} />
-
+        <Route path='/' element={<HomePage />}>
+          <Route path='/createTask' element={<CreateTask />} />
+          <Route path='/crearTarea' element={<Navigate to='/createTask' />} />
+          <Route path='/updateTask/:id' element={<UpdateTask />} />
+          <Route path='/listTasks' element={<ListTasks />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
