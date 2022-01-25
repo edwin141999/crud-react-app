27348bbef35074377d1010c7b255e25fed0bfb90
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-25 00:02:53 -0600

    add update and delete

--- src/pages/ListTasks.js
+++ src/pages/ListTasks.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getTasks } from '../services/db';
+import { useNavigate } from 'react-router-dom';
+import { deleteTask, getTasks } from '../services/db';
 
 function ListTasks() {
     const [tasks, setTasks] = useState(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
         getTasksData()
@@ -14,21 +16,25 @@ function ListTasks() {
         setTasks(tasksData.docs)
     }
 
+    const deleteAndUpdate = (id) => {
+        deleteTask(id)
+        getTasksData()
+    }
+
     return (<div>
         {
             tasks && tasks.map(e =>
                 <div className='card' key={e.id}>
-                    <p>id:{e.id}</p>
+                    {/* <p>id: {e.id}</p> */}
                     <h4 className='title'>Titulo</h4>
                     <p className='title'>{e.data().title}</p>
                     <h4 className='descripcion'>Descripcion</h4>
                     <p>{e.data().description}</p>
                     <h4 className='hecho'>Hecho</h4>
-                    <input type={'checkbox'} defaultChecked={e.data().done} />
-                    {/* `/users/$e.id` */}
+                    <input type={'checkbox'} defaultChecked={e.data().done} onClick={() => { deleteAndUpdate(e.id) }} />
                     <div>
-                        <button>Update</button>
-                        <button>Delete</button>
+                        <button onClick={() => { navigate(`/updateTask/${e.id}`) }}>Update</button>
+                        <button onClick={() => { deleteAndUpdate(e.id) }}>Delete</button>
                     </div>
                 </div>,
             )
