7d13c73ecf8d9fa940d3892aab7fa7e8a5cd9b02
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-01-25 00:02:27 -0600

    add css in create

--- src/pages/CreateTask.js
+++ src/pages/CreateTask.js
@@ -1,18 +1,19 @@
 import { useForm } from "react-hook-form";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { addTask } from "../services/db";
+import '../pages/createTask.css'
 
 function CreateTask() {
     const { register, formState: { errors }, handleSubmit } = useForm()
     const navigate = useNavigate()
     const onSubmit = (data) => {
         console.log(data);
-        addTask(data.title,data.description)
+        addTask(data.title, data.description)
         navigate('/listTasks')
     }
 
     return (
-        <div>
+        <div className="div">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <h4>Titulo</h4>
                 <input type={"text"} {...register("title", { required: true })} />
@@ -20,8 +21,6 @@ function CreateTask() {
                 <h4>Descripcion</h4>
                 <input type={"text"} {...register("description", { required: true })} />
                 <p>{errors.description?.type === 'required' && 'Se requiere una descripcion'}</p>
-                <NavLink to='/listTasks' >
-                </NavLink>
                 <button type="submit">Crear</button>
             </form>
         </div>);
